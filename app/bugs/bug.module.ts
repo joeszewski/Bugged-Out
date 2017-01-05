4d43e82744df09820399971943642ab81f81dd67
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2017-01-04 20:21:17 -0600

    set up bug-list component files and imported into the bug.module

--- app/bugs/bug.module.ts
+++ app/bugs/bug.module.ts
@@ -1,11 +1,19 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
+//Component
+import { BugListComponent } from './bug-list/bug-list.component';
+
 @NgModule({
-    imports: [ SharedModule ],
-    declarations: [ ],
-    exports: [ ],
-    providers: [ ]
+    imports: [
+        SharedModule
+    ],
+    declarations: [
+        BugListComponent
+    ],
+    exports: [],
+    providers: []
 })
 
 export class BugModule { }
