2bd2d60e91465c28c86648d8ed4b703e469ff6c1
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2017-01-04 19:02:12 -0600

    Updated Skeleton Project references:

--- app/app.component.js
+++ app/app.component.js
@@ -14,7 +14,7 @@ let AppComponent = class AppComponent {
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: '<h1>Skeleton Project</h1>'
+        template: '<h1>BUGGED OUT</h1>'
     }), 
     __metadata('design:paramtypes', [])
 ], AppComponent);
