0c981117f80756e1b6d535b9d445eabcd833b8bf
JoeSzewski <Joe@Joes-MacBook-Pro-2.local>
2017-01-06 14:22:55 -0600

    created the navbar component

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,16 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BugModule } from './bugs/bug.module';
 import { AppRoutingModule } from './app-routing.module';
 
+// Component
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-    imports: [ 
+    imports: [
         BrowserModule,
         BugModule,
         AppRoutingModule
-        ],
-    declarations: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    ],
+    declarations: [
+        AppComponent,
+        NavbarComponent
+    ],
+    bootstrap: [AppComponent]
 })
 
 export class AppModule { }
